356c74cbf18dd6968b9bad586df4f18f96518eab
nikita13122001 <113139821+nikita13122001@users.noreply.github.com>
2023-09-28 01:16:17 +0530

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About = ()=>{
+    return(<div className="mb-20">
+    <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">About</h1>
+    
+    <p className="mx-auto leading-relaxed text-base mb-4">sdncmkdnkdnvkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjd
+        vnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjkfdnfknvfkdjnv
+        kfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjk
+        fdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdn
+        vkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdnc
+        mkdnkdnvkmfdnvjkfdnfknvfkd
+        jnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvvvvvvvvv</p>
+        <p>sdncmkdnkdnvkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjd
+        vnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjkfdnfknvfkdjnv
+        kfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjk
+        fdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdn
+        vkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdnc
+        mkdnkdnvkmfdnvjkfdnfknvfkd
+        jnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvvvvvvvvv</p>
+    <p className="mx-auto leading-relaxed text-base mb-4">sdncmkdnkdnvkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjd
+        vnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjkfdnfknvfkdjnv
+        kfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjk
+        fdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdn
+        vkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdnc
+        mkdnkdnvkmfdnvjkfdnfknvfkd
+        jnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvvvvvvvvv</p>
+    <p className="mx-auto leading-relaxed text-base mb-4">sdncmkdnkdnvkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjd
+        vnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjkfdnfknvfkdjnv
+        kfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjk
+        fdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdn
+        vkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdnc
+        mkdnkdnvkmfdnvjkfdnfknvfkd
+        jnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvvvvvvvvv</p>
+</div> )
+}
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Home = ()=>{
+    return (<div className="mb-20">
+        <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">Hello , welcome to my blog</h1>
+        
+        <p className="mx-auto leading-relaxed text-base mb-4">sdncmkdnkdnvkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjd
+            vnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjkfdnfknvfkdjnv
+            kfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjk
+            fdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdn
+            vkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdnc
+            mkdnkdnvkmfdnvjkfdnfknvfkd
+            jnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvvvvvvvvv</p>
+            <p>sdncmkdnkdnvkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjd
+            vnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjkfdnfknvfkdjnv
+            kfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjk
+            fdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdn
+            vkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdnc
+            mkdnkdnvkmfdnvjkfdnfknvfkd
+            jnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvvvvvvvvv</p>
+        <p className="mx-auto leading-relaxed text-base mb-4">sdncmkdnkdnvkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjd
+            vnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjkfdnfknvfkdjnv
+            kfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjk
+            fdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdn
+            vkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdnc
+            mkdnkdnvkmfdnvjkfdnfknvfkd
+            jnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvvvvvvvvv</p>
+        <p className="mx-auto leading-relaxed text-base mb-4">sdncmkdnkdnvkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjd
+            vnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjkfdnfknvfkdjnv
+            kfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdnvkmfdnvjk
+            fdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdncmkdnkdn
+            vkmfdnvjkfdnfknvfkdjnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvsdnc
+            mkdnkdnvkmfdnvjkfdnfknvfkd
+            jnvkfdjnvkjfdnvkjdvnfkdjvnjkvnkfnvvvvvvvvv</p>
+    </div>
+    )
+}
+ export default Home
